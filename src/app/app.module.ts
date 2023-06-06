2233f91bb21655e4036bfb2e1e9fe04ea389b255
Максим <novokshonov_m@mail.ru>
2023-06-06 21:16:17 +0300

    Header,login page, accounts page, accout-info page layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AccountsPageComponent } from './pages/accounts-page/accounts-page.component';
+import { AccountComponent } from './pages/accounts-page/account/account.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccountInfoPageComponent } from './pages/account-info-page/account-info-page.component';
+import { DetailsHeaderComponent } from './components/details-header/details-header.component';
+import { NewTransferComponent } from './pages/account-info-page/new-transfer/new-transfer.component';
+import { BalanceDynamicsComponent } from './components/balance-dynamics/balance-dynamics.component';
+import { TransferHistoryComponent } from './components/transfer-history/transfer-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HeaderComponent,
+    AccountsPageComponent,
+    AccountComponent,
+    AccountInfoPageComponent,
+    DetailsHeaderComponent,
+    NewTransferComponent,
+    BalanceDynamicsComponent,
+    TransferHistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/balance-dynamics/balance-dynamics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-dynamics',
+  templateUrl: './balance-dynamics.component.html',
+  styleUrls: ['./balance-dynamics.component.scss']
+})
+export class BalanceDynamicsComponent {
+
+}
